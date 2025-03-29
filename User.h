ca85991993b06b5c401463bf39b056ebad22494a
badam2004 <adam.janos.bukovics@gmail.com>
2025-03-29 23:42:17 +0100

    ROLE_USER

--- User.h
+++ User.h
@@ -4,17 +4,22 @@
 #include <iostream>
 #include <fstream>
 
+enum Role {
+    READER, //0
+    AUTHOR, //1
+    ADMIN   //2
+};
 
 class User {
     std::string username;
-    std::string role; // "reader", "author", "admin"
+    Role role; // "reader", "author", "admin"
 public:
     User(std::string name, std::string r) : username(std::move(name)), role(std::move(r)) {}
 
-    bool canPost() const { return role == "author" || role == "admin"; }
+    bool canPost() const { return role == Role::AUTHOR || role == Role::ADMIN; }
     bool canComment() const { return true; }
-    bool canModify() const { return role == "admin";}
-    bool canDelete() const { return role == "admin";}
+    bool canModify() const { return role == Role::ADMIN; ;}
+    bool canDelete() const { return role == Role::ADMIN; ;}
     void showDashboard() const {
         std::cout << username << " irányítópultja (" << role << ")\n";
     }
